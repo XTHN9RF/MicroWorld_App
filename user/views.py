4806abee857f3d0cf4de4965fcd2c2cc506d761f
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-08 17:55:40 +0300

    feat(login) complete simple login view for testing

--- user/views.py
+++ user/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.http import HttpResponse
 
 from .forms import LoginForm
 
@@ -9,7 +12,15 @@ from .forms import LoginForm
 class LoginView(View):
     """Handles login functionallity for users"""
 
-    def get(self, request):
+    def post(self, request):
         """Handles GET requests"""
-        form = LoginForm()
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Logged in sucessfullyy', status=200)
+            else:
+                return HttpResponse('Invalid username or password', status=401)
         return render(request, 'user/login.html', {'form': form})
