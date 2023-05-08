bc8fe624b9c924f8dd4a059d7f86cb54bb70fd57
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-08 18:20:43 +0300

    feat(auth): add base urls to settings, get method for login page and index page

--- user/urls.py
+++ user/urls.py
@@ -8,5 +8,5 @@ app_name = 'user'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout'),
-    path('home/', views.HomePageView.as_view(), name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
--- user/views.py
+++ user/views.py
@@ -11,25 +11,31 @@ from .forms import LoginForm
 
 class LoginView(View):
     """Handles login functionallity for users"""
+    form = LoginForm()
 
-    def post(self, request):
+    def get(self, request):
         """Handles GET requests"""
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            user = authenticate(request, username=data['username'], password=data['password'])
-            if user is not None:
-                login(request, user)
-                return HttpResponse('Ви успішно увійшли до системи', status=200)
-            else:
-                return HttpResponse('Неправильні пароль або ім\'я користувача', status=401)
+        form = self.form
         return render(request, 'user/login.html', {'form': form})
 
 
-class HomePageView(LoginRequiredMixin,View):
+def post(self, request):
+    """Handles GET requests"""
+    form = self.form(request.POST)
+    if form.is_valid():
+        data = form.cleaned_data
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Ви успішно увійшли до системи', status=200)
+        else:
+            return HttpResponse('Неправильні пароль або ім\'я користувача', status=401)
+    return render(request, 'user/login.html', {'form': form})
+
+
+class HomePageView(LoginRequiredMixin, View):
     """Handles logic of home page view"""
 
     def get(self, request):
         """Handles GET requests"""
         return render(request, 'user/home.html')
-
