f0eb2c78c204894c4cc514c64770e93d9c4ed53c
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-08 18:34:23 +0300

    feat(auth): add password change functionallity and pages for it and its result

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 app_name = 'user'
 urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout'),
-    path('', views.HomePageView.as_view(), name='home'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name = 'user/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'user/password_change_done.html'), name='password_change_done'),
 ]
