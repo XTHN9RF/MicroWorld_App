e95ca25ebe9eb1834cebf9416de23db3193de5c3
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-08 17:23:28 +0300

    feat(user): create and add user app to project

--- MW/urls.py
+++ MW/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
 ]
