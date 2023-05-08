e0d43dd989ac56880f61b1486cc360d387914812
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-08 18:08:32 +0300

    feat(auth): add app name to use urls correctly and create base view

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+
+app_name = 'user'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout')
