49ada7d720a576fd19dee624266d11a4a506137f
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-10 00:29:34 +0300

    test(register): make password editable and test creating new user and login

--- user/models.py
+++ user/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=30, unique=True, )
     name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=150, editable=False)
+    password = models.CharField(max_length=150)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False, editable=False)
 
