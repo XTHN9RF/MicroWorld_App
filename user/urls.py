9089a09fd288ddfda3217a2a48d02acd96dfd9f0
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-10 00:27:00 +0300

    feat(registration): add registration page to app

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,7 @@ app_name = 'user'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name = 'user/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'user/password_change_done.html'), name='password_change_done'),
--- user/views.py
+++ user/views.py
@@ -7,17 +7,16 @@ from django.contrib.auth import login
 from django.http import HttpResponse
 
 from .forms import LoginForm
+from .forms import RegisterForm
 
 
 class LoginView(View):
     """Handles login functionallity for users"""
-    form = LoginForm()
+    form = LoginForm
 
     def get(self, request):
         """Handles GET requests"""
-        form = self.form
-        return render(request, 'user/login.html', {'form': form})
-
+        return render(request, 'user/login.html', {'form': self.form})
 
 def post(self, request):
     """Handles GET requests"""
@@ -39,3 +38,22 @@ class HomePageView(LoginRequiredMixin, View):
     def get(self, request):
         """Handles GET requests"""
         return render(request, 'user/home.html')
+
+
+class RegisterView(View):
+    """Handles logic of registration view"""
+    form = RegisterForm
+
+    def get(self, request):
+        """Handles GET requests"""
+        return render(request, 'user/register.html', {'form': self.form})
+
+    def post(self, request):
+        """Handles POST requests"""
+        form = self.form(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('login/')
+        return render(request, 'user/register.html', {'form': form})
