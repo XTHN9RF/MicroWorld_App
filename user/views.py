8551106da02eff9fd38eaa22a269c7023902b912
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-10 00:28:19 +0300

    refactor(login): add ident for post method that wasn	 in login class

--- user/views.py
+++ user/views.py
@@ -18,18 +18,18 @@ class LoginView(View):
         """Handles GET requests"""
         return render(request, 'user/login.html', {'form': self.form})
 
-def post(self, request):
-    """Handles GET requests"""
-    form = self.form(request.POST)
-    if form.is_valid():
-        data = form.cleaned_data
-        user = authenticate(request, username=data['username'], password=data['password'])
-        if user is not None:
-            login(request, user)
-            return HttpResponse('Ви успішно увійшли до системи', status=200)
-        else:
-            return HttpResponse('Неправильні пароль або ім\'я користувача', status=401)
-    return render(request, 'user/login.html', {'form': form})
+    def post(self, request):
+        """Handles GET requests"""
+        form = self.form(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Ви успішно увійшли до системи', status=200)
+            else:
+                return HttpResponse('Неправильні пароль або ім\'я користувача', status=401)
+        return render(request, 'user/login.html', {'form': form})
 
 
 class HomePageView(LoginRequiredMixin, View):
