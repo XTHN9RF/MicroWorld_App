924af9190cf60d63b4aba6b492a2442d31add14a
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-09 23:57:07 +0300

    feat(register): create registration form

--- user/forms.py
+++ user/forms.py
@@ -4,3 +4,22 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.EmailField(label='Email', widget=forms.EmailInput(), max_length=100)
     password = forms.CharField(label='Password', widget=forms.PasswordInput(), max_length=35)
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password']
+        widgets = {
+            'username': forms.EmailInput(),
+            'password': forms.PasswordInput(),
+        }
+        labels = {
+            'username': 'Email',
+            'password': 'Password',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+        }
+        extra_kwargs = {
+            'password': {'write_only': True,'read_only': True},
+        }
