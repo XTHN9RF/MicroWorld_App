d185a04d514b5610f1502b4793f41820fca91d8c
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-10 21:12:09 +0300

    feat(post): create post model and apply migrations, register model to admin panel

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    """A blog post model with slug field"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    slug = models.SlugField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='posts/%y/%m/%d', blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model title in admin panel"""
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """Override the save method to auto-generate a slug field"""
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
