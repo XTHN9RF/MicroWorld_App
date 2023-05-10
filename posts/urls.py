9564eb44b4ab6e9dab61104f1346449b877ff436
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-10 21:13:16 +0300

    feat(post): create view that handles post creating and add page to it

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from django.urls import include
 
+from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
+    path('create/', views.PostCreateView.as_view(), name='create'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+from .forms import PostForm
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    """A view to list all posts"""
+
+    def get(self, request):
+        """Handle GET requests"""
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'posts/create.html', context)
+
+    def post(self, request):
+        """Handle POST requests"""
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.user = request.user
+            new_post.save()
+        return render(request, 'posts/create.html', {'form': form})
