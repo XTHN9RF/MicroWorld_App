1a45d59ac6d9d28de130589d8c7ce96a8eee597a
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-15 23:37:59 +0300

    feat(style): style create post page and add it to nav, test functionallity of this page

--- posts/forms.py
+++ posts/forms.py
@@ -1,13 +1,20 @@
 from django import forms
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
     """A form to create a new post"""
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'image']
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Title'}),
-            'content': forms.Textarea(attrs={'placeholder': 'Content'}),
-            'image': forms.FileInput(attrs={'placeholder': 'Image'}),
+            'title': forms.TextInput(
+                attrs={'placeholder': 'Заголовок поста (50 символів максимум)', 'maxlength': '50', 'required': True,
+                       'class': 'bg-white border sm:text-sm rounded-lg block w-full p-2.5 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500'}),
+            'content': forms.Textarea(
+                attrs={'placeholder': 'Вміст поста (250 символів максимум)', 'maxlength': '250', 'required': True,
+                       'class': 'bg-white border sm:text-sm rounded-lg block w-full p-2.5 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500'}),
+            'image': forms.FileInput(attrs={'placeholder': 'Зображення поста', 'required': True,
+                                            'class': 'text-white sm:text-sm rounded-lg block w-full p-2.5 border-gray-600 placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500'}),
         }
