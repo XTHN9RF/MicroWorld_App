337f023a8b51e115df7f80363f007e4833d45771
Vladyslav Duran <justez4rtz@gmail.com>
2023-05-18 23:33:00 +0300

    feat: create friend app and model of request

---
+++ friends/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from django.conf import settings
+
+class FriendRequest(models.Model):
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='to_user', on_delete=models.CASCADE)
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='from_user', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Від {self.from_user.email} користувачу {self.to_user.email}"
